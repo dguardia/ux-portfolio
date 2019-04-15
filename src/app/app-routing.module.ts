d0355b874c74fe5c4d7e8864b3e874ff89dfe93f
David Guardia <david.guardia@gmail.com>
2019-04-15 01:27:18 -0400

    Add readme

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProjectOneComponent } from './project-one/project-one.component';
+import { ProjectTwoComponent } from './project-two/project-two.component';
+import { ProjectThreeComponent } from './project-three/project-three.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  { path:  'project-one', component:  ProjectOneComponent},
+  { path:  'project-two', component:  ProjectTwoComponent},
+  { path:  'project-three', component:  ProjectThreeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [HomeComponent, ProjectOneComponent, ProjectTwoComponent, ProjectThreeComponent]
---
+++ src/app/project-three/project-three.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-project-three',
+  templateUrl: './project-three.component.html',
+  styleUrls: ['./project-three.component.scss']
+})
+export class ProjectThreeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
